37da42b1af1f7b180f5d7c584699fd33da18398f
evonne777 <1527988166@qq.com>
2025-08-05 10:24:14 +0800

    first commit

---
+++ src/components/tabbar/TabBarItem.vue
@@ -0,0 +1,51 @@
+<!--  -->
+<template>
+  <div class="tab-bar-item">
+    <!-- <img src="../../assets/img/tabbar/home.svg" alt="">
+    <div>首页</div> -->
+    <slot name='item-icon'></slot>
+    <slot name="item-text"></slot>
+    <!-- <div class="tab-bar-item">
+      <img src="../../assets/img/tabbar/home.svg" alt="">
+      首页
+    </div>
+    <div class="tab-bar-item">
+      <img src="../../assets/img/tabbar/home.svg" alt="">
+      分类
+    </div>
+    <div class="tab-bar-item">
+      <img src="../../assets/img/tabbar/home.svg" alt="">
+      购物车
+    </div>
+    <div class="tab-bar-item">
+      <img src="../../assets/img/tabbar/home.svg" alt="">
+      我的
+    </div> -->
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+    }
+  }
+}
+</script>
+
+<style  scoped>
+
+  .tab-bar-item {
+    flex: 1;
+    text-align: center;
+    height: 49px;
+    font-size: 14px;
+  }
+  .tab-bar-item img {
+    width: 24px;
+    height: 24px;
+    margin-top: 3px;
+    margin-bottom: 2px;
+  }
+
+</style>
